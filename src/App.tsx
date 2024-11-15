332cdf18a5878e2b3626cb1e3e073b197bd1f943
Thao NGUYEN <phuongthaolyon@gmail.com>
2024-11-16 00:03:03 +0100

    add createHabit page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/globalStyle.css";
+import { CreateHabit } from "./components/CreateHabit";
 
+function App() {
   return (
-  <h1>Hello</h1>
-  )
+    <h1>
+      <CreateHabit />
+    </h1>
+  );
 }
 
-export default App
+export default App;
